263c15ff26e8ccef21c6ee058bc11cb99654e5ad
Maks <mandesero@gmail.com>
2022-10-11 16:23:54 +0300

    sem 5

--- 20221011/2/prog.py
+++ 20221011/2/prog.py
@@ -1,36 +1,44 @@
 from math import *
+from fractions import Fraction
+from decimal import *
+data = input().split()
+w, h, a, b = map(int, data[:-1])
+func = lambda x: eval(data[-1])
+def func(x):
+    try:
+        return eval(data[-1])
+    except Exception:
+        return func(x + 1/1000)
 
-func = lambda x: eval(sstr[4])
-def fun(x):
-	return func(x)
 
-def getmax(func, A, B, W):
-	max = fun(A)
-	for i in range(0, W+1):
-		max = func(A+i*(B-A)/W) if func(A+i*(B-A)/W) > max else max
-	return max
+dx = Fraction(f"{b - a}/{w}")
 
-def getmin(func, A, B, W):
-	min = fun(A)
-	for i in range(0, W+1):
-		min = func(A+i*(B-A)/W) if func(A+i*(B-A)/W) < min else min
-	return min
+def olala(screen):
+    for i in range(len(screen[0]) - 1):
+        stolb1 = [screen[j][i] for j in range(len(screen))]
+        start_1 = stolb1.index('*')
 
-sstr = list(str(input()).split(" "))
-W, H, A, B = int(sstr[0]), int(sstr[1]), int(sstr[2]), int(sstr[3])
-C, D = getmin(fun, A, B, W), getmax(fun, A, B, W)
-C -= (D-C)/H
-xstep = (B-A)/W
-ystep = (D-C)/H
-#print(A, B, C, D, xstep, ystep)
-y = D
-while y>C:
-	x = A
-	while x<B:
-		if abs(fun(x)-y) < 0.5*((abs(D-C))/(H)+ystep*(abs(B-A))/(W)):
-			print("*", end="")
-		else:
-			print(" ", end="")
-		x += xstep
-	print("")
-	y -= ystep
+        stolb2 = [screen[j][i + 1] for j in range(len(screen))]
+        stop_1 = stolb2.index('*')
+        for j in range(min(start_1, stop_1) + 1, max(start_1, stop_1)):
+            screen[j][i] = '*'
+
+
+
+
+
+
+t = [func(dx * i) for i in range(-w // 2, w // 2 + 1)]
+if min(t) < 0:
+    a = abs(min(t))
+    t = [elem + a for elem in t]
+length = max(t) - min(t)
+tmp = [Decimal(float(elem / length * (h - 1))).quantize(Decimal(1.))  for elem in t]
+
+field = [[" " for _ in range(w)] for _ in range(h)]
+for i in range(w):
+    field[h - 1 - int(tmp[i])][i] = '*'
+
+olala(field)
+for line in field:
+    print(*line, sep='')
--- 20221011/3/prog.py
+++ 20221011/3/prog.py
@@ -1,4 +1,28 @@
-def mysub(x, y):
-    return type(x)(elem for elem in x if elem not in y) if isinstance(x, (tuple, list)) else x - y
+import sys
+s = sys.stdin.read()
+nr = s.count('.')
+nw = s.count('~')
+s = s.splitlines()
+a, b = len(s), len(s[0])
+new_a = a - 2
+new_nw = nw // new_a if nw % new_a == 0 else nw // new_a + 1
+new_nr = nr // new_a
+field = "#" * a + "\n"
+for _ in range(new_nr):
+    field += "#" + "." * new_a + "#\n"
 
-print(mysub(*eval(input())))
+for _ in range(new_nw):
+    field += "#" + "~" * new_a + "#\n"
+
+field += "#" * a
+
+print(field)
+nr = field.count('.')
+nw = field.count('~')
+length = len(str(nr + nw) + str(max(nr, nw))) + 1
+if nr >= nw:
+    print(("."*20).ljust(20), f"{nr}/{nr + nw}".rjust(length))
+    print(("~"*round(nw/nr*20)).ljust(20), f"{nw}/{nr + nw}".rjust(length))
+else:
+    print(("."*round(nr/nw*20)).ljust(20), f"{nr}/{nr + nw}".rjust(length))
+    print(("~"*20).ljust(20), f"{nw}/{nr + nw}".rjust(length))
