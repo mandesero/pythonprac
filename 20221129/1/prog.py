156e9829df61f6f5c4241f6add312429ebb82d2e
Maks <mt27@mail.ru>
2022-11-29 13:55:50 +0300

    u

--- 20221129/1/prog.py
+++ 20221129/1/prog.py
@@ -6,14 +6,8 @@ class dump(type):
     @staticmethod
     def wrapped(func):
         def inner(*args, **kwargs):
-            if func.__name__ != "my_setattr":
-                try:
-                    if not isinstance(args[0], (int, float, str, bool)):
-                        print(f"{func.__name__}: {args[1:]}, {kwargs}")
-                    else:
-                        print(f"{func.__name__}: {args}, {kwargs}")
-                except IndexError:
-                    print(f"{func.__name__}: {args}, {kwargs}")
+            print(
+                f"{func.__name__}: {tuple(elem for elem in args if isinstance(elem, (int, float, str, bool)))}, {kwargs}")
             return func(*args, **kwargs)
 
         return inner
@@ -28,16 +22,6 @@ class dump(type):
                 new_cls_dct[attr] = val
 
         return super().__new__(mcs, name, bases, new_cls_dct)
-
-    @classmethod
-    def __prepare__(mcs, name, _):
-        def my_setattr(self, attrname, value):
-            if callable(value):
-                self.__dict__[attrname] = dump.wrapped(value)
-            else:
-                self.__dict__[attrname] = value
-
-        return {'__setattr__': my_setattr}
 
 
 if __name__ == '__main__':
