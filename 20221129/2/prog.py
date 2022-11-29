f35da9123ce212a491af3aaa7fd18899dcbf08cc
Maks <mt27@mail.ru>
2022-11-29 12:07:41 +0300

    u

--- 20221129/2/prog.py
+++ 20221129/2/prog.py
@@ -1,6 +1,24 @@
 import sys
+from inspect import get_annotations
 
 
-a = sys.stdin.buffer.read()
-a = a.decode('utf-8').encode("latin-1").decode('cp1251', errors='replace')
-sys.stdout.buffer.write(a.encode('utf-8'))
+class check(type):
+    def __new__(mcs, name, bases, classdict):
+        def check_annotations(self, *args):
+            annotations = get_annotations(self.__class__)
+            for field, tpy in annotations.items():
+                try:
+                    if not isinstance(getattr(self, field), tpy):
+                        break
+                except AttributeError:
+                    break
+            else:
+                return True
+            return False
+
+        classdict["check_annotations"] = check_annotations
+        return super().__new__(mcs, name, bases, classdict)
+
+
+if __name__ == '__main__':
+    exec(sys.stdin.read())
