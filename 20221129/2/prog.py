a81e8b3c37e14486e6c86b69ab64e773d4faa1bb
Maks <mandesero@gmail.com>
2022-12-06 11:44:26 +0300

    update urls

--- 20221129/2/prog.py
+++ 20221129/2/prog.py
@@ -1,21 +1,21 @@
 import sys
 from inspect import get_annotations
+from typing import get_origin
 
 
 class check(type):
     def __new__(mcs, name, bases, classdict):
-        def check_annotations(self, *args):
+        def check_annotations(self):
             annotations = get_annotations(self.__class__)
             for field, tpy in annotations.items():
                 try:
-                    if not isinstance(getattr(self, field), tpy):
-                        break
+                    attr = getattr(self, field)
                 except AttributeError:
-                    break
-            else:
-                return True
-            return False
-
+                    return False
+                attr_type = tpy if get_origin(tpy) is None else get_origin(tpy)
+                if not isinstance(attr, attr_type):
+                    return False
+            return True
         classdict["check_annotations"] = check_annotations
         return super().__new__(mcs, name, bases, classdict)
 
