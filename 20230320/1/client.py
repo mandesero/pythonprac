81906642156cf8b197f80294b296a9e2023ef771
mandesero <mandesero@gmail.com>
2023-03-25 14:04:35 +0300

    Добавлен многопользовательский MUD (multiuser multiuser dungeon).

--- 20230320/1/client.py
+++ 20230320/1/client.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+
+
 import cowsay as cs
 import shlex
 import cmd
 import sys
 import socket
+import asyncio
+import threading
+import readline
 
 from custom_monster import cust_mstr
 from defaults import WEAPONS
@@ -11,18 +17,6 @@ list_cows = cs.list_cows() + ["jgsbat"]
 
 
 class Game(cmd.Cmd):
-    @staticmethod
-    def send_recv_server(msg):
-        s.send((msg.strip() + "\n").encode())
-        ans = s.recv(1024).decode().strip().replace("'", "")
-
-        if len(t := ans.split("\n")) == 3:
-            print(cs.cowsay(message=t[1], cowfile=cust_mstr)) if t[
-                1
-            ] == "jgsbat" else print(cs.cowsay(message=t[1], cow=t[2]))
-        else:
-            print(ans)
-
     def do_addmon(self, args):
         """
         Добавление монстра на поле.
@@ -39,23 +33,23 @@ class Game(cmd.Cmd):
 
         if len(args := shlex.split(args)) == 8 and args[0] in list_cows:
             msg = "addmon " + shlex.join(args)
-            Game.send_recv_server(msg)
+            s.send((msg.strip() + "\n").encode())
         elif args[0] not in list_cows:
             print("Cannot add unknown monster")
         else:
             print("Invalid arguments")
 
     def do_up(self, args):
-        Game.send_recv_server("up")
+        s.send(("up\n").encode())
 
     def do_down(self, args):
-        Game.send_recv_server("down")
+        s.send(("down\n").encode())
 
     def do_left(self, args):
-        Game.send_recv_server("left")
+        s.send(("left\n").encode())
 
     def do_right(self, args):
-        Game.send_recv_server("right")
+        s.send(("right\n").encode())
 
     def do_attack(self, args):
         """
@@ -71,13 +65,19 @@ class Game(cmd.Cmd):
         match args := shlex.split(args):
             case [t, "with", weapon]:
                 if weapon in WEAPONS:
-                    Game.send_recv_server(" ".join(["attack", t, str(WEAPONS[weapon])]))
+                    s.send(
+                        (" ".join(["attack", t, str(WEAPONS[weapon])]) + "\n").encode()
+                    )
                 else:
                     print("Unknown weapon")
             case [t]:
-                Game.send_recv_server(" ".join(["attack", t, str(WEAPONS["sword"])]))
+                s.send((" ".join(["attack", t, str(WEAPONS["sword"])]) + "\n").encode())
             case _:
                 print("Invalid arguments")
+
+    def do_quit(self, args):
+        s.send("quit\n".encode())
+        self.onecmd("exit")
 
     def do_exit(self, args):
         """
@@ -86,14 +86,35 @@ class Game(cmd.Cmd):
         return 1
 
 
+def get_reponse():
+    while True:
+        ans = s.recv(2048).decode()
+        if ans:
+            if ans.strip() == "Goodbye":
+                break
+
+            print("\n" + ans + "\n")
+            print(
+                f"\n{cmdline.prompt}{readline.get_line_buffer()}",
+                end="",
+                flush=True,
+            )
+
+
 def main():
     print("<<< Welcome to Python-MUD 0.1 >>>")
-    print(s.recv(1024).decode().strip())
+    print("Active session:")
+    print(s.recv(1024).decode())
+
+    global cmdline
+    cmdline = Game()
+    gm = threading.Thread(target=get_reponse, args=())
+    gm.start()
     Game().cmdloop()
 
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((sys.argv[1], int(sys.argv[2]) if len(sys.argv) > 2 else 1337))
-        s.send("Connect\n".encode())
+        s.connect(("localhost", 1338))
+        s.send(f"{sys.argv[1]}\n".encode())
         main()
--- 20230320/1/server.py
+++ 20230320/1/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import cowsay as cs
 import shlex
 import asyncio
@@ -8,22 +10,43 @@ from defaults import WEAPONS
 
 class Client:
     client_list = {}
+
     def __init__(self, name, addr):
         self.name = name
         self.addr = addr
-        client_list.update({name: self})
-    
+        self.hero = None
+        self.writer = None
+        Client.client_list.update({name: self})
+
     @staticmethod
     def connect(name, addr):
         if name in Client.client_list:
-            return None
-        
-        return Client(name, addr)
+            return False
+
+        _ = Client(name, addr)
+        return True
 
     @staticmethod
     def meta(name):
         if name in Client.client_list:
             return Client.client_list[name]
+
+    @staticmethod
+    def disconnect(name):
+        if Client.meta(name):
+            Client.client_list.pop(name)
+
+    def __str__(self):
+        return f"""
+        Name: {self.name}
+        Addr: {self.addr}
+        Pos : {self.hero.pos}
+        """
+
+    def broadcast(self, msg):
+        for _, obj in filter(lambda u: u[0] != self.name, Client.client_list.items()):
+            obj.writer.write(msg)
+
 
 list_cows = cs.list_cows() + ["jgsbat"]
 
@@ -51,9 +74,10 @@ class Game:
         "left": (-1, 0),
     }
 
-    def __init__(self, hero):
-        self.field = [[None] * 10 for _ in range(10)]
-        self.hero = hero
+    field = [[None] * 10 for _ in range(10)]
+
+    def __init__(self, player):
+        self.player = player
 
     def add_monster(self, monster):
         i, j = monster.coords
@@ -70,18 +94,21 @@ class Game:
 
     def change_hero_pos(self, way):
         x, y = Game.ways[way]
-        i = self.hero.pos[0] = (x + self.hero.pos[0]) % 10
-        j = self.hero.pos[1] = (y + self.hero.pos[1]) % 10
 
-        msg = [f"Moved to ({i}, {j})"]
+        i = self.player.hero.pos[0] = (x + self.player.hero.pos[0]) % 10
+        j = self.player.hero.pos[1] = (y + self.player.hero.pos[1]) % 10
+
+        msg = f"Moved to ({i}, {j})"
         if self.field[i][j]:
-            msg += self.encounter(i, j)
+            text, name = self.encounter(i, j)
+            msg += cs.cowsay(message=text, cow=name)
 
         return msg
 
     def attack(self, pos, name, dmg):
         msg = "No monster here"
         i, j = pos
+        flag = False
         if monster := self.field[i][j]:
             if monster.name == name:
                 dmg = dmg if monster.hp > dmg else monster.hp
@@ -94,61 +121,87 @@ class Game:
                     self.field[i][j] = None
                 else:
                     msg += f"\n{monster.name} now has {monster.hp} hp"
+                flag = True
             else:
                 msg = f"No {name} here"
 
-        return msg
+        return msg, flag
 
 
 async def echo(reader, writer):
     host, port = writer.get_extra_info("peername")
-    player = Hero()
-    dungeon = Game(player)
+    print(f"New connection from {host}:{port}")
+    usr = await reader.readline()
+    usr = usr.decode().strip()
 
-    while not reader.at_eof():
-        data = await reader.readline()
-        msg = shlex.split(data.decode().strip())
-        ans = ""
-        print(msg)
-        match msg:
-            case way if len(way) == 1 and way[0] in Game.ways:
-                ans = "\n".join(dungeon.change_hero_pos(way[0]))
+    if not Client.connect(usr, f"{host}:{port}"):
+        writer.write(f"User with login {usr} already exists.\n".encode())
+        print(f"Disconnect {host}:{port}")
+        writer.close()
+        await writer.wait_closed()
 
-            case ["addmon", *args]:
-                print("Addmon")
-                if len(args) == 8:
-                    if args[0] in list_cows:
-                        ans = dungeon.add_monster(
-                            Monster(
-                                args[0],
-                                args[args.index("hello") + 1],
-                                int(args[args.index("hp") + 1]),
-                                (
-                                    int(args[args.index("coords") + 1]),
-                                    int(args[args.index("coords") + 2]),
-                                ),
+    else:
+        clt = Client.meta(usr)
+        clt.hero = Hero()
+        clt.writer = writer
+        writer.write(str(Client.meta(usr)).encode())
+        clt.broadcast(("\n New user:\n" + str(Client.meta(usr))).encode())
+
+        dungeon = Game(clt)
+        while not reader.at_eof():
+            data = await reader.readline()
+            msg = shlex.split(data.decode().strip())
+            ans = ""
+            print(msg)
+            match msg:
+                case way if len(way) == 1 and way[0] in Game.ways:
+                    ans = dungeon.change_hero_pos(way[0])
+                    writer.write(ans.encode())
+                    await writer.drain()
+
+                case ["addmon", *args]:
+                    print("Addmon")
+                    if len(args) == 8:
+                        if args[0] in list_cows:
+                            ans = dungeon.add_monster(
+                                Monster(
+                                    args[0],
+                                    args[args.index("hello") + 1],
+                                    int(args[args.index("hp") + 1]),
+                                    (
+                                        int(args[args.index("coords") + 1]),
+                                        int(args[args.index("coords") + 2]),
+                                    ),
+                                )
                             )
-                        )
+                            print(ans)
+                            clt.broadcast((clt.name + ": " + ans).encode())
 
-            case ["attack", *args]:
-                print("Attack")
-                ans = dungeon.attack(player.pos, args[0], int(args[1]))
+                case ["attack", *args]:
+                    print("Attack")
+                    ans = dungeon.attack(clt.hero.pos, args[0], int(args[1]))
+                    print(ans)
+                    if ans[1]:
+                        clt.broadcast((clt.name + ": " + ans[0]).encode())
+                    else:
+                        writer.write(ans[0].encode())
+                        await writer.drain()
 
-            case ["Connect"]:
-                ans = "<<< Welcome to Python-MUD 0.1 >>>"
+                case ["quit"]:
+                    break
 
-            case _:
-                ans = "Error"
+                case _:
+                    ans = "Error"
 
-        writer.write(ans.encode())
-        await writer.drain()
-
-    writer.close()
-    await writer.wait_closed()
+        clt.broadcast(("Dissconnect:\n" + str(clt)).encode())
+        writer.write("Goodbye".encode())
+        Client.disconnect(usr)
+        writer.close()
+        await writer.wait_closed()
 
 
 async def main():
-    server = await asyncio.start_server(echo, "0.0.0.0", 1337)
+    server = await asyncio.start_server(echo, "0.0.0.0", 1338)
     async with server:
         await server.serve_forever()
 
